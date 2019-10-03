7cca7f0e8b0c175907bcd0ab62c57fe56e861a73
Kaito <mail.kaito03@gmail.com>
2019-10-03 16:03:43 +0900

    complete create template script

--- create.sh
+++ create.sh
@@ -5,9 +5,10 @@ printf "Input Overview >> "; read OVERVIEW
 cp ./template.cc ${FILE}
 
 # Replace
-sed -i -e "s/FILENAME/"${FILE}"/g" ${FILE} && \rm ${FILE}
-sed -i -e "s/OVERVIEW/"${OVERVIEW}"/g" ${FILE} && \rm ${FILE}
-# sed -i -e "s/DATE/"$(date)"/g" ${FILE}
+sed -i -e "s/FILENAME/"${FILE}"/g" ${FILE}
+sed -i -e "s/OVERVIEW/"${OVERVIEW}"/g" ${FILE}
+DATE=$(date | sed "s/年 /-/g" | sed "s/月 /-/g" | sed "s/日.*//g")
+sed -i -e "s/DATE/"${DATE}"/g" ${FILE}
 
 # delete backup file
 \rm ${FILE}-e
