d41b1c58c44e195f2d573f914af0820390445225
Kaito <mail.kaito03@gmail.com>
2019-10-07 13:08:51 +0900

    update ignore

---
+++ Ex02/ex02.cc
@@ -0,0 +1,13 @@
+/*
+    ex02.cc
+    概要: Ex02: 性名の入出力
+    Author: Kaito Kimura
+    Date: 2019-10-07
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
