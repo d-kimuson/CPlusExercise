ac47cc471e5ea3fcff400508092ea9a23939b378
Kaito <mail.kaito03@gmail.com>
2019-10-15 05:36:04 +0900

    add little description about test script

--- test.sh
+++ test.sh
@@ -3,12 +3,15 @@
 # Usage
 # 1. COPY this script into Ex0X Dir.
 # 2. RUN chmod a+x test.sh
-# 3.
+# 3. INPUT RUN script into RUN_EXERCISE_SCRIPT like "./a.out"
+# 4. ./test.sh: if there is no outputs => Success!!
 
+alias RUN_EXERCISE_SCRIPT="./a.out < list3.txt"  # sample for ex03
+
+
+# RUN TEST
 if test -e "tmp.txt"; then
   \rm tmp.txt
 fi
-
-alias RUN_EXERCISE_SCRIPT="./a.out < list3.txt"  # sample for ex03
 
 g++ ex03.cc && RUN_EXERCISE_SCRIPT > tmp.txt && diff tmp.txt result3.txt
